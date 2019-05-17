7770d3a9ea6de8000986bfec2da51b953a8a89d4
orcasuit <orcasuit@gmail.com>
2019-05-17 16:54:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/part000recyclerviewcardview/Student.java
@@ -0,0 +1,19 @@
+package com.example.part000recyclerviewcardview;
+
+public class Student {
+    private String mName;
+    private String mNumber;
+    private String mDepartment;
+
+    public String getDepartment() { return mDepartment; }
+
+    public void setDepartment(String department) { mDepartment = department;}
+
+    public String getName() { return mName; }
+
+    public void setName(String name) { mName = name; }
+
+    public String getNumber() { return mNumber; }
+
+    public void setNumber(String number) { mNumber = number; }
+}
